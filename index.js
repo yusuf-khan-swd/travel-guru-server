a52675e2280a26fe61987cac943f58006fd00e78
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-23 11:29:04 +0600

    Create a Hotels API and filter by placesId

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const places = require('./data/places.json');
+const hotels = require('./data/hotels.json');
 
 app.get('/', (req, res) => {
   res.send('Travel Guru Server is running');
@@ -20,6 +21,12 @@ app.get('/places/:id', (req, res) => {
   const id = req.params.id;
   const hotel = places.find(ht => ht.id === parseInt(id));
   res.send(hotel);
+});
+
+app.get('/hotels/:placesId', (req, res) => {
+  const placesId = req.params.placesId;
+  const availableHotels = hotels.filter(ht => ht.placesId === parseInt(placesId));
+  res.send(availableHotels);
 });
 
 app.listen(port, () => {
